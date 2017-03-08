f5b5ebc4a79cc913d993a5b488fcddc9c3fff680
d0cHolland <wcholland@gmail.com>
2017-03-08 13:04:51 -0500

    Unified Group Provisioning

--- provision.ps1
+++ provision.ps1
@@ -1,15 +1,51 @@
-param(
+param(    
     [Parameter(Mandatory=$true)]
-    [string]$SiteUrl,
+    [string]$ProjectCode,
+    
     [Parameter(Mandatory=$true)]
-    [string]$Username,
-    [Parameter(Mandatory=$true)]
-    [securestring]$Password
-)
-$CurrentDate = Get-Date -Format FileDateTime
-Set-SPOTraceLog -On -Level Debug -LogFile ".\TraceLogs\$($CurrentDate).log"
-$Credentials = New-Object System.Management.Automation.PSCredential($Username, $Password)
-Connect-PnPOnline -Url $SiteUrl -Credentials $Credentials
+    [string]$ProjectName,
+    
+    [Parameter(Mandatory=$false)]
+    [string]$GroupDescription,
+    
+    [Parameter(Mandatory=$false)]
+    [string[]]$GroupOwners,
+    
+    [Parameter(Mandatory=$false)]
+    [string[]]$GroupMembers,
 
-Apply-SPOProvisioningTemplate -Path ".\schema.xml"
+    [switch]$Log
+)
+Set-StrictMode -Version 5.0
+if($Log){        
+    Set-SPOTraceLog -On -Level Debug -LogFile ".\TraceLogs\$(Get-Date -Format FileDateTime).log"
+}
+
+if([String]::IsNullOrWhiteSpace($GroupDescription)){
+    $GroupDescription = "Office 365 Group for the $ProjectName ($ProjectCode) project."     
+}
+
+try{    
+    Write-Host "Connecting to Microsoft Graph..." -NoNewline
+    Connect-PnPMicrosoftGraph -Scopes "Group.ReadWrite.All","User.Read.All"    
+    Write-Host "Connected!" -ForegroundColor Green
+    
+    Write-Host "Creating Unified Group..." -NoNewline
+    $NewUnifiedGroup = New-PnPUnifiedGroup -DisplayName $ProjectName -Description $GroupDescription -MailNickname $ProjectCode -Owners $GroupOwners -Members $GroupMembers
+    Write-Host "Done!" -ForegroundColor Green
+
+    Write-Host "Connecting to new Site '$($NewUnifiedGroup.SiteUrl)'..." -NoNewline
+    Connect-PnPOnline $NewUnifiedGroup.SiteUrl -Credentials $(Get-Credential -Message "Enter Credentials for $($NewUnifiedGroup.SiteUrl)")
+    Write-Host "Connected!" -ForegroundColor Green
+
+    Write-Host "Adding standard lists..." -NoNewline
+    Apply-SPOProvisioningTemplate -Path ".\schema.xml"
+    Write-Host "Completed!" -ForegroundColor Green
+}
+catch{
+    #Have to catch and throw error yourself to stop the script from executing.
+    Write-Host "Failed" -ForegroundColor Red    
+    throw $_     
+}
+
 
